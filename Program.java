d349d6b38ba1998688927c397d24b7a6f7872ece
banupriya-15 <45116357+banupriya-15@users.noreply.github.com>
2023-01-23 16:11:33 -0500

    ast

---
+++ Program.java
@@ -0,0 +1,21 @@
+package ast;
+
+import java.io.PrintStream;
+
+public class Program extends ASTNode {
+
+    final Stmt stmt;
+
+    public Program(Stmt stmt, Location loc) {
+        super(loc);
+        this.stmt = stmt;
+    }
+
+    public Stmt getStmt() {
+        return stmt;
+    }
+
+    public void println(PrintStream ps) {
+        //ps.println(expr);
+    }
+}
